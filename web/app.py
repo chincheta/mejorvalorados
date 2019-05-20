8b89997feed1120d2ed5f95c403c4c401ab33f23
chincheta <dev@chincheta.com>
2019-05-20 22:13:02 +0200

    Fix bug while merging comments from multiple sources.

--- web/app.py
+++ web/app.py
@@ -8,15 +8,13 @@ mongo_host = os.getenv('MONGO_HOST') or 'localhost'
 
 @app.route('/')
 def index():
-    comments = []
     mongo = MongoClient(mongo_host)
 
-    comments = \
-        mongo['elmundoes-bot']['comments'].find().sort('heat', -1)
-    comments.append(
-        mongo['okdiariocom-bot']['comments'].find().sort('heat', -1))
+    elmundo = mongo['elmundoes-bot']['comments'].find().sort('heat', -1)
+    okdiario = mongo['okdiariocom-bot']['comments'].find().sort('heat', -1)
+
     mongo.close()
-    return render_template("comments.html", comments=comments)
+    return render_template("comments.html", elmundo=elmundo, okdiario=okdiario)
 
 
 if __name__ == '__main__':
